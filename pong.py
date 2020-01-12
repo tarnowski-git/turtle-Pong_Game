617ec8d1d98de716e160ee7bfa1b2a815f36e3b2
Konrad Tarnowski <k.tarnowski@student.uksw.edu.pl>
2020-01-12 01:35:11 +0100

    Create two moving paddles.

--- pong.py
+++ pong.py
@@ -1,10 +1,60 @@
 import turtle
 
 
+def setup_window(window):
+    window.title("Pong Game")
+    window.setup(width=800, height=600)
+    # accelerate time program
+    window.tracer(0)
+    window.colormode(255)
+    window.bgcolor("green")
+
+
+class Paddle():
+
+    def __init__(self, position):
+        self.turtle = turtle.Turtle()
+        self.turtle.shape("square")
+        self.turtle.color("white")
+        self.turtle.shapesize(stretch_wid=10, stretch_len=1)
+        # erase the turtle pathway
+        self.turtle.penup()
+        self.turtle.goto(position, 0)
+
+    def go_up(self):
+        y = self.turtle.ycor()
+        y += 30
+        if y < 250:
+            self.turtle.sety(y)
+
+    def go_down(self):
+        y = self.turtle.ycor()
+        y -= 30
+        if y > -250:
+            self.turtle.sety(y)
+
+
+def bind(window, a, b):
+    window.listen()
+    window.onkeypress(a.go_up, "w")
+    window.onkeypress(a.go_down, "s")
+
+    window.onkeypress(b.go_up, "Up")
+    window.onkeypress(b.go_down, "Down")
+
+
+def game(window):
+    a = Paddle(350)
+    b = Paddle(-350)
+    bind(window, a, b)
+    while True:
+        window.update()
+
+
 def main():
-    game = turtle.Screen()
-    # end program on click
-    game.exitonclick()
+    window = turtle.Screen()
+    setup_window(window)
+    game(window)
 
 
 main()
