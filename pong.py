6ddc21ca107d8f5170ff3b85c099f7b4acfbf7c3
Konrad Tarnowski <k.tarnowski@student.uksw.edu.pl>
2020-01-12 14:56:58 +0100

    Create ball colision.

--- pong.py
+++ pong.py
@@ -34,21 +34,58 @@ class Paddle():
             self.turtle.sety(y)
 
 
+class Ball():
+
+    def __init__(self):
+        self.turtle = turtle.Turtle()
+        self.turtle.shape("circle")
+        self.turtle.color("red")
+        # erase the turtle pathway
+        self.turtle.penup()
+        self.turtle.goto(0, 0)
+        # init move vector
+        self.dx = 1
+        self.dy = 1
+
+    def move(self):
+        self.turtle.setx(self.turtle.xcor()+self.dx)
+        self.turtle.sety(self.turtle.ycor()+self.dy)
+
+    def colision(self):
+        # up and down (height=600)
+        if self.turtle.ycor() > 280:
+            self.turtle.sety(280)
+            self.dy *= -1
+        if self.turtle.ycor() < -280:
+            self.turtle.sety(-280)
+            self.dy *= -1
+
+        # left and right (width=800)
+        if self.turtle.xcor() > 380:
+            self.turtle.setx(380)
+            self.dx *= -1
+        if self.turtle.xcor() < -380:
+            self.turtle.setx(-380)
+            self.dx *= -1
+
+
 def bind(window, a, b):
     window.listen()
     window.onkeypress(a.go_up, "w")
     window.onkeypress(a.go_down, "s")
-
     window.onkeypress(b.go_up, "Up")
     window.onkeypress(b.go_down, "Down")
 
 
 def game(window):
-    a = Paddle(350)
-    b = Paddle(-350)
+    a = Paddle(-350)
+    b = Paddle(350)
+    ball = Ball()
     bind(window, a, b)
     while True:
         window.update()
+        ball.move()
+        ball.colision()
 
 
 def main():
