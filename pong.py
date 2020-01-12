71bb0400fb2eba4911988b951d5debfbd8d6d172
Konrad Tarnowski <k.tarnowski@student.uksw.edu.pl>
2020-01-12 21:57:45 +0100

    Create working score table

--- pong.py
+++ pong.py
@@ -11,6 +11,25 @@ def setup_window(window):
     window.bgcolor("green")
 
 
+class Pencil():
+
+    def __init__(self):
+        self.turtle = turtle.Turtle()
+        self.turtle.hideturtle()
+        self.turtle.color("white")
+        self.turtle.shapesize(stretch_wid=10, stretch_len=1)
+        # erase the turtle pathway
+        self.turtle.penup()
+        self.turtle.goto(0, 250)
+        self.turtle.write("Player A: 0 Player B: 0",
+                          align="center", font=("Arial", 30))
+
+    def display(self, ponits_A, points_B):
+        self.turtle.clear()
+        self.turtle.write("Player A: {} Player B: {}".format(
+            ponits_A, points_B), align="center", font=("Arial", 30))
+
+
 class Paddle():
 
     def __init__(self, position):
@@ -37,7 +56,7 @@ class Paddle():
 
 class Ball():
 
-    def __init__(self, a, b):
+    def __init__(self, a, b, pencil):
         self.turtle = turtle.Turtle()
         self.turtle.shape("circle")
         self.turtle.color("red")
@@ -49,6 +68,9 @@ class Ball():
         self.dy = 0.5
         self.a = a
         self.b = b
+        self.pencil = pencil
+        self.ponits_A = 0
+        self.ponits_B = 0
 
     def move(self):
         self.turtle.setx(self.turtle.xcor()+self.dx)
@@ -59,6 +81,7 @@ class Ball():
         if self.turtle.ycor() > 290:
             self.turtle.sety(290)
             self.dy *= -1
+
         if self.turtle.ycor() < -290:
             self.turtle.sety(-290)
             self.dy *= -1
@@ -67,9 +90,14 @@ class Ball():
         if self.turtle.xcor() > 390:
             self.turtle.setx(390)
             self.dx *= -1
+            self.ponits_A += 1
+            self.pencil.display(self.ponits_A, self.ponits_B)
+
         if self.turtle.xcor() < -390:
             self.turtle.setx(-390)
             self.dx *= -1
+            self.ponits_B += 1
+            self.pencil.display(self.ponits_A, self.ponits_B)
 
         # paddles
         if self.turtle.xcor() < -330 and math.fabs(self.turtle.ycor() - self.a.turtle.ycor()) < 50:
@@ -92,7 +120,8 @@ def bind(window, a, b):
 def game(window):
     a = Paddle(-350)
     b = Paddle(350)
-    ball = Ball(a, b)
+    d = Pencil()
+    ball = Ball(a, b, d)
     bind(window, a, b)
     while True:
         window.update()
